1867ac50c7dbedda7d8d69fb82f29b68bafe731a
kihiuFrank <franklinekihiu@gmail.com>
2022-09-27 11:16:15 +0300

    ethersjs in nextjs / react

---
+++ pages/index.js
@@ -0,0 +1,152 @@
+import Head from "next/head";
+import Image from "next/image";
+import { useState } from "react";
+import styles from "../styles/Home.module.css";
+import { ethers } from "ethers";
+
+// connect to metamask
+// execute a funtion
+
+export default function Home() {
+  const [isConnected, setIsConnected] = useState(false);
+  const [signer, setSigner] = useState();
+
+  async function connect() {
+    if (typeof window.ethereum !== "undefined") {
+      try {
+        await ethereum.request({ method: "eth_requestAccounts" });
+        setIsConnected(true);
+        let connectedProvider = new ethers.providers.Web3Provider(
+          window.ethereum
+        );
+        setSigner(connectedProvider.getSigner());
+      } catch (error) {
+        console.log(error);
+      }
+    } else {
+      setIsConnected(false);
+    }
+  }
+
+  async function execute() {
+    // we need
+    // contract address
+    // contract ABI
+    // functions
+    // node connection (metamask)
+    if (typeof window.ethereum !== "undefined") {
+      const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+      const abi = [
+        {
+          inputs: [
+            {
+              internalType: "string",
+              name: "_name",
+              type: "string",
+            },
+            {
+              internalType: "uint256",
+              name: "_favoriteNumber",
+              type: "uint256",
+            },
+          ],
+          name: "addPerson",
+          outputs: [],
+          stateMutability: "nonpayable",
+          type: "function",
+        },
+        {
+          inputs: [
+            {
+              internalType: "string",
+              name: "",
+              type: "string",
+            },
+          ],
+          name: "nameToFavouriteNumber",
+          outputs: [
+            {
+              internalType: "uint256",
+              name: "",
+              type: "uint256",
+            },
+          ],
+          stateMutability: "view",
+          type: "function",
+        },
+        {
+          inputs: [
+            {
+              internalType: "uint256",
+              name: "",
+              type: "uint256",
+            },
+          ],
+          name: "people",
+          outputs: [
+            {
+              internalType: "uint256",
+              name: "favouriteNumber",
+              type: "uint256",
+            },
+            {
+              internalType: "string",
+              name: "name",
+              type: "string",
+            },
+          ],
+          stateMutability: "view",
+          type: "function",
+        },
+        {
+          inputs: [],
+          name: "retrieve",
+          outputs: [
+            {
+              internalType: "uint256",
+              name: "",
+              type: "uint256",
+            },
+          ],
+          stateMutability: "view",
+          type: "function",
+        },
+        {
+          inputs: [
+            {
+              internalType: "uint256",
+              name: "_favoriteNumber",
+              type: "uint256",
+            },
+          ],
+          name: "store",
+          outputs: [],
+          stateMutability: "nonpayable",
+          type: "function",
+        },
+      ];
+      const contract = new ethers.Contract(contractAddress, abi, signer);
+      try {
+        await contract.store(42);
+      } catch (error) {
+        console.log(error);
+      }
+    } else {
+      console.log("Please install MetaMask");
+    }
+  }
+
+  return (
+    <div className={styles.container}>
+      Hello Kihiu.
+      {isConnected ? (
+        <>
+          "Connected!"
+          <button onClick={() => execute()}>Execute</button>
+        </>
+      ) : (
+        <button onClick={() => connect()}>Connect!</button>
+      )}
+    </div>
+  );
+}
